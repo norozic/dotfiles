3cccdfa36c0ead9b5ec5f772f9f6dfc7fed829d9
motiya <54768914+mtiyz@users.noreply.github.com>
2020-04-20 17:45:38 +0900

    :sparkles: add install start question

--- bin/install.sh
+++ bin/install.sh
@@ -1,19 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+
+function choise {
+  while true; do
+    echo -n "$1 [Y/n]: "
+    read ANS
+    case $ANS in
+      [Yy]*)
+        return 0;;
+      [Nn]*)
+        return 1;;
+      *)
+        ;;
+    esac
+  done
+}
+
+function start () {
+  if choise "Do you want to start install for $1?"; then
+    echo "Start"
+  else
+    echo "Quit" && exit 0
+  fi
+}
 
 if [ -f /etc/os-release ]; then
   OS_RELEASE=`cat /etc/os-release`
   case $OS_RELEASE in
-    *ubuntu* ) DIST="Ubuntu";;
-    *debian* ) DIST="Debian";;
-    *centos* ) DIST="CentOS";;
-    *fedora* ) DIST="Fedora";;
-    *rhel* ) DIST="RHEL";;
-    *arch* ) DIST="ArchLinux";;
-    *opensuse* ) DIST="OpenSUSE";;
-    * ) echo "Sorry, We don't support this distribution";;
+    *ubuntu* ) start "Ubuntu";;
+    *debian* ) start "Debian";;
+    *centos* ) start "CentOS";;
+    *fedora* ) start "Fedora";;
+    *rhel* ) start "RHEL";;
+    *arch* ) start "ArchLinux";;
+    *opensuse* ) start "OpenSUSE";;
+    * ) echo "Sorry, We don't support this distribution" && exit 1;;
   esac
 else
-  echo "Sorry, We cannot find /etc/os-release"
+  echo "Sorry, We don't support this distribution" && exit 1
 fi
-
-echo $DIST
