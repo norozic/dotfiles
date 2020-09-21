a9e54fed29b8a9b2d17982d62f503ebb30ed7fb1
noroz <54768914+norozic@users.noreply.github.com>
2020-09-21 12:19:52 +0900

    🐛 enable force option of symbolic link

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -11,7 +11,7 @@ echo 'Installing tmux config...'
 if [ -f ~/.tmux.conf ]; then
   rm ~/.tmux.conf
 fi
-ln -s $DOTPATH/config/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $DOTPATH/config/tmux/.tmux.conf ~/.tmux.conf
 
 if ! sudo grep zsh /etc/shells; then
   sudo bash -c "echo `which zsh` | sudo tee -a /etc/shells"
@@ -24,5 +24,5 @@ fi
 if [ -f ~/.tmux.session.conf ]; then
   rm ~/.tmux.session.conf
 fi
-ln -s $DOTPATH/config/tmux/.tmux.session.conf ~/.tmux.session.conf
+ln -sf $DOTPATH/config/tmux/.tmux.session.conf ~/.tmux.session.conf
 
